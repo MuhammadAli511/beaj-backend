bd4217a212056b6cc231cd4f0aacceae8372c193
Muhammad Ali <mohammadali417@outlook.com>
2024-06-27 03:41:52 +0500

    Change port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import sequelize from './config/sequelize.js';
 import cors from 'cors';
 import routes from './routes/index.js';
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 const app = express();
 
