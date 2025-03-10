fc2170d0d143d21de2e0f1b7f20876fa7d827674
salmanmehmood1 <salmanarain106@gmail.com>
2025-03-11 00:55:49 +0500

    SSH Tunnel

--- controllers/chatBotController.js
+++ controllers/chatBotController.js
@@ -1,9 +1,55 @@
 import service from '../services/chatBotService.js';
+import dotenv from 'dotenv';
+dotenv.config();
+import axios from 'axios';
 
 const webhookController = async (req, res, next) => {
     try {
-        const { body } = req;
+        if (process.env.ENVIRONMENT != 'DEV') {
+        if (
+            body.entry &&
+            body.entry[0].changes &&
+            body.entry[0].changes[0].value.messages &&
+            body.entry[0].changes[0].value.statuses == undefined
+        ) {
+        const message = req.body.entry[0].changes[0].value.messages[0];
+        const phone_number = "+" + message.from;
+
+        const salmanEndpoint = "https://smiling-pro-sheep.ngrok-free.app";
+        const aliEndpoint = "https://sensibly-solid-aardvark.ngrok-free.app";
+        if (phone_number == "+923012232148") {
+            try {
+                const response = await axios.post(salmanEndpoint, req.body, {
+                    headers: req.headers, 
+                });
+    
+                return res.json(response.data); 
+            } catch (error) {
+                console.error("Request failed:", error.message);
+                return; 
+            }
+        }
+        else if (phone_number == "+923225036358") {
+            try {
+                const response = await axios.post(aliEndpoint, req.body, {
+                    headers: req.headers, 
+                });
+    
+                return res.json(response.data); 
+            } catch (error) {
+                console.error("Request failed:", error.message);
+                return; 
+            }
+        }
+        else{
+            await service.webhookService(body, res);
+        }
+       }
+     } 
+    else{
         await service.webhookService(body, res);
+    }
+        
     } catch (error) {
         error.fileName = 'chatBotController.js';
         next(error);
@@ -22,36 +68,7 @@ const verifyWebhookController = async (req, res, next) => {
 const uploadUserDataController = async (req, res, next) => {
     try {
         const { users } = req.body;
-        // let phone_number = req.body.phone_number;
-        // const salmanEndpoint = "https://smiling-pro-sheep.ngrok-free.app";
-        // const aliEndpoint = "";
-        // if (phone_number === "+923012232148") {
-            
-            
-        //     try {
-        //         const response = await axios.post(salmanEndpoint, req.body, {
-        //             headers: req.headers, 
-        //         });
-    
-        //         return res.json(response.data); 
-        //     } catch (error) {
-        //         console.error("Request failed:", error.message);
-        //         return; 
-        //     }
-        // }
-        // else if (phone_number === "+923225036358") {
-        //     try {
-        //         const response = await axios.post(aliEndpoint, req.body, {
-        //             headers: req.headers, 
-        //         });
-    
-        //         return res.json(response.data); 
-        //     } catch (error) {
-        //         console.error("Request failed:", error.message);
-        //         return; 
-        //     }
-        // }
-        // res.status(400).json({ message: "Phone number does not match" });
+       
 
         const count = await service.uploadUserDataService(users);
         res.status(200).send({ message: `Successfully uploaded ${count} users.` });
