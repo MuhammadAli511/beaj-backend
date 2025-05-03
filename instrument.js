b6f5ddff34b1852f295079ec4796a7a6564b0acc
MuhammadAli511 <mohammadali417@outlook.com>
2025-05-03 22:55:14 +0500

    Sentry Integration

---
+++ instrument.js
@@ -0,0 +1,7 @@
+import * as Sentry from "@sentry/node"
+
+Sentry.init({
+    dsn: "https://e348721bdec5ca74b4136fef91ef1924@o4509260481560576.ingest.us.sentry.io/4509260487000064",
+    tracesSampleRate: 1.0,
+    sendDefaultPii: true,
+});
